696e23081daacdb9e352eef1be455dbc55201b99
Stephen Mount <stemount@gmail.com>
2016-10-23 08:14:56 +0100

    updates to nodejs

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express'),
     app = express(),
     emojiFavicon = require('emoji-favicon'),
-    minifyHTML = require('express-minify-html'),
-    minify = require('html-minifier').minify;
+    minifyHTML = require('express-minify-html');
 
+// Set computer emoji as favicon.
 app.use(emojiFavicon('computer'));
 
+// minify HTML.
 app.use(minifyHTML({
     override:      true,
     htmlMinifier: {
@@ -19,14 +20,18 @@ app.use(minifyHTML({
     }
 }));
 
+// use EJS templating.
 app.set('view engine', 'ejs');
 
+// serve homepage.
 app.get('/', function (req, res, next) {
   res.render('homepage');
 });
 
-app.use(express.static(__dirname));
+// set static assets to 1 year max-age expiry.
+app.use(express.static(__dirname, {maxage: '1y'}));
 
+// listen on 3000.
 app.listen(3000, function () {
   console.log('Site is on 3000!');
 });
